54ce0cca034ff12336ebd4772aa047eb194b64b3
lucasoliari <63376493+lucasoliari@users.noreply.github.com>
2025-01-18 13:38:42 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { Pool } = require('pg');
 
 // Configurações do Servidor
